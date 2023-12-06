1973e2e7781a1ca9430ca24d2e91aa7ac88a5f2f
Yanex18 <129259386+Yanex18@users.noreply.github.com>
2023-12-06 17:48:43 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Yanex18/projet-info1.git
+git push -u origin main
+int tab[20][10]
+int tab[]={}
+int visuel(){
+    
+}
+
+int main(){
+
+}
