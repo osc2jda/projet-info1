6eb81da193edd7dba2591f9838607ced6c33bae8
Yanex18 <129259386+Yanex18@users.noreply.github.com>
2023-12-08 09:30:20 +0100

    Create representation.c

---
+++ representation.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+char snoopy() {
+    printf("%c",'$');
+    return 0;
+}
+char balle(){
+    printf("%c",'*');
+    return 0;
+}
+char oiseaux() {
+    printf("%c",'~');
+    return 0;
+}
+char blocs_poussables(){
+    printf("%c",'=');
+    return 0;
+}
+char blocs_cassables() {
+    printf("%c",'=');
+    return 0;
+}
+char blocs_pieges(){
+    printf("%c",'=');
+    return 0;
+}
